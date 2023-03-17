050eef9f3950868a68fcbbec253453b993bd230a
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2023-03-17 10:55:45 +0000

    Add string question and character question and remove character

---
+++ ChangeString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            string y;
+            
+            Console.WriteLine("Please write a string");
+            x= Console.ReadLine();
+            Console.WriteLine("Please write a caracter");
+            y= Console.ReadLine();
+            var removechar = new string[] {y};
+            foreach (var z in removechar)
+            {
+                x = x.Replace(y, string.Empty);
+            }
+            Console.WriteLine(x);
+            
+
+
+        }
+    }
+}
