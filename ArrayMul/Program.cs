96210229d0f2a8aa1150ab58181086d1e836918b
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-06 16:03:53 +0000

    Write in console with space

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -18,3 +18,18 @@ namespace ArrayMul
             float[] b = new float[2] {float.Parse(args[4], dec), float.Parse(args[5], dec)};
             float [] resultado = new float[2];
 
+            for (int i = 0; i <2; i++)
+            {
+                resultado[i] = 0;
+                for (int j = 0; j < 2; j++)
+                {
+                    resultado[i] += A[i,j] * b[j];
+                }
+            }
+            foreach (float valor in resultado)
+            {
+                Console.WriteLine($"| {valor.ToString(" 0.00;-0.00", dec),7} |");
+            }
+        }
+    }
+}
