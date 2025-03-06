89e5cede815d81e6b3759104964c291ce13c6b87
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-06 10:54:57 +0000

    Added code for conting of characters

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RighSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    return;
+                }
+                if (args.Length > 3)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+    
+        }
+    }
+}
