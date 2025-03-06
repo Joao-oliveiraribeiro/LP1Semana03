5e4992c6a65222e512a02dd76dc53509f77fd428
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-06 15:52:45 +0000

    Create first matrix

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo dec = CultureInfo.InvariantCulture;
+
+            float[,] A = new float[2, 2]
+            {
+                {float.Parse(args[0], dec), float.Parse(args[1], dec)},
+                {float.Parse(args[2], dec), float.Parse(args[3], dec)}
+            };
+
+
+}
