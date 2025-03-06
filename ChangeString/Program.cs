0be6fa75192f9b84760bef96ec3bed9976c47b50
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-06 10:58:07 +0000

    Added code for string input

---
+++ ChangeString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           Console.Write("Digite uma string: ");
+           string texto = Console.ReadLine(); 
+        }
+    }
+}
