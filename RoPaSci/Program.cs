c91998099d6c739510e0c9f59d8c2d1caebd5831
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-07 12:43:52 +0000

    Modify code in main

---
+++ RoPaSci/Program.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+
+namespace RoPaSci
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameItem player1 = Enum.Parse<GameItem>(args[0]);
+            GameItem player2 = Enum.Parse<GameItem>(args[1]);
+            GameStatus result = RockPaperScissors(player1, player2);
+
+            switch (result)
+            {
+                case GameStatus.Draw:
+                    Console.WriteLine("It's a draw!");
+                    break;
+                case GameStatus.Player1Wins:
+                    Console.WriteLine("Player 1 wins!");
+                    break;
+                case GameStatus.Player2Wins:
+                    Console.WriteLine("Player 2 wins!");
+                    break;
+            }
+        }
+
+
+    }
+}
